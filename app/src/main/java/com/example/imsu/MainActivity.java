74cf670348cd06c96de1f70f247a97b33e929a7b
Sandeep <chansanfdo@gmail.com>
2019-08-07 23:31:39 +0530

    Third Commit IMSU

--- app/src/main/java/com/example/imsu/MainActivity.java
+++ app/src/main/java/com/example/imsu/MainActivity.java
@@ -16,10 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {
-        System.out.println("Clicked ScoreBoard");
+    // loads ScoreBoard
+    public void loadScoreBoard(View view) {
+        System.out.println(getResources().getString(R.string.clicked_btn_Scoreboard));
 
-        // Get the view from activity_main.xml
+        // Get the view from activity_scoreboard.xml
         setContentView(R.layout.activity_scoreboard);
 
         // Get the correct button
@@ -27,6 +28,37 @@ public class MainActivity extends AppCompatActivity {
 
         // Start ScoreBoardActivity.class
         Intent myIntent = new Intent(MainActivity.this, ScoreBoardActivity.class);
+        startActivity(myIntent);
+    }
+
+    // loads Settings
+    public void loadSettings(View view) {
+        System.out.println(getResources().getString(R.string.clicked_btn_Settings));
+
+        // Get the view from activity_settings.xml
+        setContentView(R.layout.activity_settings);
+
+        // Get the correct button
+        button = findViewById(R.id.btn_Settings);
+
+        // Start SettingsActivity.class
+        Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(myIntent);
+    }
+
+
+    // loads Help
+    public void loadHelp(View view) {
+        System.out.println(getResources().getString(R.string.clicked_btn_Help));
+
+        // Get the view from activity_help.xml
+        setContentView(R.layout.activity_help);
+
+        // Get the correct button
+        button = findViewById(R.id.btn_help);
+
+        // Start HelpActivity.class
+        Intent myIntent = new Intent(MainActivity.this, HelpActivity.class);
         startActivity(myIntent);
     }
 }
