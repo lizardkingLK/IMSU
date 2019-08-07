41026fba659a5fe9548b31d9fe68af8fa3b2f878
Sandeep <chansanfdo@gmail.com>
2019-08-07 21:03:58 +0530

    Second Commit IMSU

--- app/src/main/java/com/example/imsu/MainActivity.java
+++ app/src/main/java/com/example/imsu/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.imsu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        System.out.println("Clicked ScoreBoard");
+
+        // Get the view from activity_main.xml
+        setContentView(R.layout.activity_scoreboard);
+
+        // Get the correct button
+        button = findViewById(R.id.btn_ScoreBoard);
+
+        // Start ScoreBoardActivity.class
+        Intent myIntent = new Intent(MainActivity.this, ScoreBoardActivity.class);
+        startActivity(myIntent);
     }
 }
---
+++ app/src/main/java/com/example/imsu/ScoreBoardActivity.java
@@ -0,0 +1,14 @@
+package com.example.imsu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScoreBoardActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from new_activity.xml
+        setContentView(R.layout.activity_scoreboard);
+    }
+}
