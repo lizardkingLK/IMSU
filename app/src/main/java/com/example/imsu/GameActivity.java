f24d6522e7c159d5ca0c4c919c25b0ff007f75f7
Sandeep <chansanfdo@gmail.com>
2019-08-15 20:53:38 +0530

    Fourteenth Commit IMSU

--- app/src/main/java/com/example/imsu/GameActivity.java
+++ app/src/main/java/com/example/imsu/GameActivity.java
@@ -1,31 +1,47 @@
 package com.example.imsu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class GameActivity extends AppCompatActivity {
-    private Player currentPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        final ImageView imgView1,imgView2,imgView3;
+        final ImageView imgView1,imgView2,imgView3,imgView4_earth,imgView5_skb;
         final TextView textView_playerName;
 
         // set current player
-        currentPlayer = Player.getInstance();
+        Player currentPlayer = Player.getInstance();
 
         // set value for prisms
         imgView1 = findViewById(R.id.imgView_prism_1);
         imgView2 = findViewById(R.id.imgView_prism_2);
         imgView3 = findViewById(R.id.imgView_prism_3);
+        imgView4_earth = findViewById(R.id.imgView_earth);
+        imgView5_skb = findViewById(R.id.imgView_skb);
+
+        Animation animFadeOut = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+        Animation animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+
+        // load skb
+        animFadeIn.reset();
+        imgView5_skb.clearAnimation();
+        imgView5_skb.startAnimation(animFadeIn);
+
+        // rotating earth
+        Animation rotate = AnimationUtils.loadAnimation(GameActivity.this, R.anim.rotate_imsu);
+        rotate.setFillAfter(true);
+        imgView4_earth.startAnimation(rotate);
 
         // set player name
         textView_playerName = findViewById(R.id.txtView_playerName);
@@ -60,6 +76,6 @@ public class GameActivity extends AppCompatActivity {
                 Prism.rotateClockWise(imgView3);
             }
         });
-
     }
+
 }
