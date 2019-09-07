0aa3464419bb2cd25c81fde5d5e93fc042732872
Sandeep <chansanfdo@gmail.com>
2019-09-07 16:07:19 +0530

    TwentyFirst Commit IMSU

--- app/src/main/java/com/example/imsu/FragmentLevelCompleted.java
+++ app/src/main/java/com/example/imsu/FragmentLevelCompleted.java
@@ -3,17 +3,17 @@ package com.example.imsu;
 
 import android.os.Bundle;
 
+import android.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentLevelCompleted extends Fragment {
+public class FragmentLevelCompleted extends DialogFragment {
 
     public FragmentLevelCompleted() {
         // Required empty public constructor
